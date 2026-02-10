aee4594ce9656529664e91955297f9eada1c775a
joshedge0 <joshdedge@gmail.com>
2026-02-09 17:03:17 -0800

    udpated config for static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html' // makes SvelteKit output a single-page index.html
+    })
+  }
+};
 
 export default config;
